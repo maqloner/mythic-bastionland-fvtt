57408f9c64bc1f674a8c049b15fbcddfaf6914c6
MrTheBino <mrthebino@gmail.com>
2025-07-03 08:41:38 +0200

    fixed referee gm button in scene navigation for foundry 13

--- module/system/render-navigation.js
+++ module/system/render-navigation.js
@@ -1,12 +1,25 @@
 import { showGMDashboard } from "../applications/gm-dashboard.js";
 
-export const renderNavigation = async (html) => {
-  if (game.user.isGM) {
-    const button = document.createElement("a");             
-    $(button).addClass("mythic-bastionland nav-item gm-dashboard");
-    $(button).html(`<i class="fas fa-dice-d20"></i> ${game.i18n.localize("MB.Dashboard.Label")} `);
+export const renderNavigation = async () => {
 
-    html.prepend(button);
+  if (document.querySelector(".mythic-bastionland.ui-control.gm-dashboard")) {
+    // If the buttons already exist, do not add them again  
+    return;
+  }
+
+  const sceneNavigation = document.querySelector("#ui-left-column-2");
+  
+  if (game.user.isGM) {
+    const navElement = document.createElement("nav");
+    navElement.classList.add("flexcol","gm-dashboard-nav-item");
+
+    const button = document.createElement("a");       
+    button.classList.add("mythic-bastionland", "ui-control", "gm-dashboard");      
+    button.innerHTML = `<i class="fas fa-dice-d20"></i> ${game.i18n.localize("MB.Dashboard.Label")} `;
+    button.style.width = "100%"; // Ensure the button takes full width, some css would be great but don't understand at present the new scene navigation system and it's styles
+
+    sceneNavigation.prepend(navElement);
+    navElement.append(button);
 
     button.addEventListener("click", () => {
       showGMDashboard();
